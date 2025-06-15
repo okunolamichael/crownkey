14df60e9fc1bb3569afeb7b45abd16d6663d8a8f
okunolamichael <michaeloolamide@gmail.com>
2025-06-15 03:42:03 -0700

    Home Screen implementation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,52 @@
+import { useGlobalContext } from "@/app/lib/global-provider";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Search from "@/components/Search";
+import { FeaturedCard, Card } from "@/components/Cards";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+  const { user } = useGlobalContext();
 
-      <Text className="font-bold my-10 font-rubik text-2xl">Welcome to Crownkey Real Esate</Text>
-    </View>
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-12 rounded-full"
+            />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-sm font-rubik text-black-100">
+                Good morning,{" "}
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                {user?.name}
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <FeaturedCard />
+        <Card />
+      </View>
+    </SafeAreaView>
   );
 }
